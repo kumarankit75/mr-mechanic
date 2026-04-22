f6358d60c8e9276cb255264a6b45e3c2f7b898df
kumarankit75 <kumarankitgovindchak@gmail.com>
2026-04-22 17:36:31 +0530

    connected frontend to live backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,32 @@
+// import axios from 'axios'
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// })
+
+// // Automatically attach token to every request if logged in
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token')
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+
+// export default API
+
+
+
+
+
+
+
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 })
 
-// Automatically attach token to every request if logged in
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
